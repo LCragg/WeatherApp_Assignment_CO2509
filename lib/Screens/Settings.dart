c2c114f90c9dd59709cda1f340c5c5e080e344f3
LCragg <Lcragg1@Uclan.ac.uk>
2022-03-08 16:52:55 +0000

    Import of Location file

--- lib/Screens/Settings.dart
+++ lib/Screens/Settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutterweatherui/Screens/Weather_app.dart';
 import 'package:flutterweatherui/main.dart';
+import 'Locations.dart';
 import 'package:flutter/material.dart';
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:get/get.dart';
