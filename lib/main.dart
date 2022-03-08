bf0d5f28ad42693c34a9a1093511f7ba84c2c29c
LCragg <Lcragg1@Uclan.ac.uk>
2022-03-08 16:49:53 +0000

    Removal of unnecessary packages and code

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutterweatherui/Screens/Settings.dart' as settings;
 import 'Screens/Weather_app.dart';
 import 'package:get/get.dart';
-import 'package:adaptive_theme/adaptive_theme.dart';
 
-void main() => runApp(GetMaterialApp(
-      title: "Weather App",
-      home: MyApp(),
-      themeMode: ThemeMode.system,
-      theme: settings.ThemeClass.lightTheme,
-      darkTheme: settings.ThemeClass.darkTheme,
-    ));
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  //create stateful widget for the application
-  const MyApp({Key? key}) : super(key: key);
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return WeatherApp(); //weather app file used as the main source for code.
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainWeatherApp(
+        currentcity: "Chorley",
+      ),
+    );
   }
 }
